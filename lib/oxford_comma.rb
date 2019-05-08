4a3e9e494bf8202d3e22eda4af725866ca70dbef
Roysan Easo <rjeaso@icloud.com>
2019-05-08 02:51:45 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+    if array.count == 1
+        return array.pop
+    elsif array.count == 2
+        return array.join(" and ")
+    else
+    last_word = array.pop
+    phrase = array.join(", ")
+    phrase << ", and #{last_word}"
+    phrase
+    end
 end
